7a5154758f1243b11327a8692553543464375036
Maden.Madina@outlook.com <Maden.Madina@outlook.com>
2021-08-07 19:57:07 +0600

    +

--- WareHouse/Sorted.cs
+++ WareHouse/Sorted.cs
@@ -20,30 +20,24 @@ namespace WareHouse1
         {
             var sortedProductsMax3 = product.OrderByDescending(u => u.Count).ThenBy(u => u.Name);
             var result = sortedProductsMax3.Take(3);
-            /* int i = 0;
-             foreach (var u in sortedProductsMax3)
-             {
-                 if (i == 3) break;
-                 Console.WriteLine($"{u.Name} - {u.Count}");
-                 i++;
-             }
-            */
-
         }
-   /*     public void SortedNotBulk (List<Product> product)
-        {
 
-            var selectedWareHouse = product.SelectMany(u => u.Languages,
-(u, l) => new { User = u, Lang = l })
-.Where(u => u.Lang == "английский" && u.User.Age < 28)
-.Select(u => u.User);
+
+        public void SortedNotBulk (List<Product> product)
+        {
+           var result = product.All(u => !(u.Products is BulkProduct));
         }
    
-        public void SortedAverageCount(List<Product> product, WareHouse WareHouse1, WareHouse WareHouse2)
+        public void SortedAverageCount(List<Product> product)
         {
-            var result = product.WareHouse1.Union(product.WareHouse2);
+            var result = ;
+            foreach (var u in result)
+            {
+                var averageCount = ;
+                Console.WriteLine($"{u.Name}, {averageCount}");
+            }
 
         }
-   */
+   
     }
 }
